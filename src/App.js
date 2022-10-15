3f7855017bd6cf7286f8e6d5bd8fd30d7f2daa69
drewell7 <91854055+drewell7@users.noreply.github.com>
2022-10-15 02:38:49 -0600

    header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import HeartLogo from './heart.svg';
 import './App.css';
+import Header from './Header';
+
+const message = 'cool cud';
+
+const Heart = (props) => {
+  return (
+    <div className='heart'>
+      <img className='heart-img' src={HeartLogo} alt='heart' />
+      <p className='heart-message'>{props.msg}</p>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Heart msg={message} />;
+    </>
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import './App.css';
+
+function Header() {
+  return ( 
+    <div className='App-header'>I ❤️ React</div>
+  )
+};
+
+export default Header;
